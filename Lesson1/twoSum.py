4409b494ca2678ebee7b011a1a2e8a93b14dc8a4
Jonty Sinai <jontysinai@gmail.com>
2018-04-17 10:40:38 +0100

    one-pass solution for twoSum ~O(n)

---
+++ Lesson1/twoSum.py
@@ -0,0 +1,38 @@
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a 
+specific target. You may assume that each input would have exactly one solution, and you 
+may not use the same element twice.
+"""
+
+def twoSum(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+
+    n = len(nums)
+    nums_set = set(nums)
+
+    for idx in range(n):
+        dif = target - nums[idx]
+
+        if dif in nums_set:
+            dif_idx = nums.index(dif)
+            if idx != dif_idx:
+                if idx < dif_idx:
+                    return [idx, dif_idx]
+                else:
+                    return [dif_idx, idx]
+                break
+
+
+nums = [3,4,3]
+target = 6
+
+ans = twoSum(nums=nums, target=target)
+print(ans)
+
+
+
+
